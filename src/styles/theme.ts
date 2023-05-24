1d384cdc568cc7300be8a95747d53f9273b305f8
2yunseong <dbsdltjd3701@naver.com>
2023-05-24 20:38:19 +0900

    feat: 초기 스타일 가이드 설정
    Co-authored-by: Nangkyeong Lim <nangkyeonglim@users.noreply.github.com>

---
+++ src/styles/theme.ts
@@ -0,0 +1,43 @@
+import { DefaultTheme } from 'styled-components';
+
+interface StyleType {
+  [key: string]: string;
+}
+
+interface FontType {
+  family: string;
+  weight: number;
+  size: number;
+  lineHeight: number;
+}
+
+const colors: StyleType = {
+  yellow100: 'ffd600',
+  yellow200: '#fae15f',
+  yellow300: '#fde982',
+  disable_yellow100: '#ede5bc',
+  white: '#ffffff',
+};
+
+function FONT({ family, weight, size, lineHeight }: FontType): string {
+  return `
+  font-family:${family};
+    font-weight : ${weight};
+    font-size : ${size}rem;
+    line-height : ${lineHeight}rem;
+    `;
+}
+
+const fonts: StyleType = {
+  h1: FONT({
+    family: 'Noto Sans KR, sans-serif',
+    weight: 600,
+    size: 4,
+    lineHeight: 5.8,
+  }),
+};
+
+export const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
