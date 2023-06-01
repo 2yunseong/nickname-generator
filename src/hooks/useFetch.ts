024d5d2c1f8ec71240643abc93f0bab65c2a32a5
2yunseong <dbsdltjd3701@naver.com>
2023-06-01 16:10:01 +0900

    fix: eslint 오류 수정

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -43,6 +43,7 @@ export const useFetch = <T>(url: string): FetchRes<T> => {
       }
       setIsLoading(false);
     })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { data, isLoading, isError };
